751af20a8156235e16fbfe32eddb941dc661aa3d
tuananh100199 <66501490+tuananh100199@users.noreply.github.com>
2022-11-12 20:57:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const bodyEl = document.querySelector("body");
+
+bodyEl.addEventListener("mousemove", (event)=>{
+   const xPosition = event.offsetX
+   const ypoistion = event.offsetY
+   const spanEl = document.createElement("span");
+
+   spanEl.style.left = xPosition + "px";
+   spanEl.style.top = ypoistion + "px";
+
+   const size = Math.random()*120;
+   spanEl.style.width = size + "px";
+   spanEl.style.height = size + "px";
+
+   bodyEl.appendChild(spanEl);
+
+   setTimeout(()=>{
+    spanEl.remove();
+   }, 3000);
+});
